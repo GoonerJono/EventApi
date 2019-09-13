b43ca53b7b1ed1fafdf4b7df2274e8b525367b23
windbag33 <44021832+windbag33@users.noreply.github.com>
2019-09-13 09:32:57 -0700

    Add files via upload

---
+++ greenqueuesystem/queuingsystem/Queue.vb
@@ -0,0 +1,35 @@
+﻿Public Class Queue
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label3.Text = TimeOfDay
+        Label6.Text = DateString
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+
+        Dim que As Integer
+
+        For que = 1 To 100
+
+        Next
+
+        Label4.Text = que + 1
+
+
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+
+        Dim que As Integer
+        For que = 1 To 100
+        Next
+
+        Label5.Text = que + 2
+
+
+
+    End Sub
+
+
+End Class
